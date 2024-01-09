4a260dad241f6561e22cce83e174c36fa9029fea
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-01-09 09:04:27 -0300

    implementa camada de api

--- autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/api/AutomovelAPI.java
+++ autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/api/AutomovelAPI.java
@@ -1,7 +1,13 @@
 package br.com.mrtech.autocenter.automoveis.application.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/v1/automovel")
 public interface AutomovelAPI {
-}
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    AutomovelIdResponse postCadastraNovoVeiculo(@RequestBody @Valid AutomovelRequest automovelRequest);
+    }
