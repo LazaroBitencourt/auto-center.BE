60c8cbafdf7927df07898630dd2d6ab775e07773
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-01-10 08:28:43 -0300

    cria contrato de controle em service

--- autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/service/AutomovelService.java
+++ autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/service/AutomovelService.java
@@ -2,7 +2,12 @@ package br.com.mrtech.autocenter.automoveis.application.service;
 
 import br.com.mrtech.autocenter.automoveis.application.api.AutomovelIdResponse;
 import br.com.mrtech.autocenter.automoveis.application.api.AutomovelRequest;
+import br.com.mrtech.autocenter.automoveis.application.api.ListAutomoveisResponse;
+
+import java.util.List;
 
 public interface AutomovelService {
     AutomovelIdResponse cadastraNovoAutomovel(AutomovelRequest automovelRequest);
+
+    List<ListAutomoveisResponse> listaTodosVeiculos();
 }
