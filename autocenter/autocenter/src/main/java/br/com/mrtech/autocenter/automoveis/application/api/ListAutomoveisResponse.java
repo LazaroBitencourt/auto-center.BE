69329863d8b81784a0e19b0353cc8956f6cd1145
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-01-10 08:53:33 -0300

    cria construtor de automovel na classe de list-response e converte para o tipo list-automovel o list-automovel-response para persistencia no bd

--- autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/api/ListAutomoveisResponse.java
+++ autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/api/ListAutomoveisResponse.java
@@ -1,8 +1,12 @@
 package br.com.mrtech.autocenter.automoveis.application.api;
 
 
+import br.com.mrtech.autocenter.automoveis.domain.Automovel;
 import br.com.mrtech.autocenter.automoveis.domain.TipoAutomovel;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class ListAutomoveisResponse {
@@ -11,4 +15,15 @@ public class ListAutomoveisResponse {
     private TipoAutomovel tipo;
     private String placa;
     private int valorDiaria;
+
+    public static List<ListAutomoveisResponse> converte(List<Automovel> automovel) {
+        return automovel.stream().map(ListAutomoveisResponse::new).collect(Collectors.toList());
+    }
+
+    public ListAutomoveisResponse(Automovel automovel) {
+        this.idAutomovel = automovel.getIdAutomovel();
+        this.tipo = automovel.getTipo();
+        this.placa = automovel.getPlaca();
+        this.valorDiaria = automovel.getValorDiaria();
+    }
 }
