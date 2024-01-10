d6c152f5fe1a13ea7bd1b6fad2c06b361e11ada9
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-01-10 08:32:57 -0300

    Testa fluxo application-service, ok

--- autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/service/AutomovelApplicationService.java
+++ autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/service/AutomovelApplicationService.java
@@ -2,11 +2,14 @@ package br.com.mrtech.autocenter.automoveis.application.service;
 
 import br.com.mrtech.autocenter.automoveis.application.api.AutomovelIdResponse;
 import br.com.mrtech.autocenter.automoveis.application.api.AutomovelRequest;
+import br.com.mrtech.autocenter.automoveis.application.api.ListAutomoveisResponse;
 import br.com.mrtech.autocenter.automoveis.application.repository.AutomovelRepository;
 import br.com.mrtech.autocenter.automoveis.domain.Automovel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Log4j2
@@ -20,5 +23,13 @@ public class AutomovelApplicationService implements AutomovelService{
         Automovel automovel = repository.salva(new Automovel((automovelRequest)));
         log.info("[finaliza] AutomovelAplicationService - cadastraNovoAutomovel");
         return AutomovelIdResponse.builder().idAutomovel(automovel.getIdAutomovel()).build();
+    }
+
+    @Override
+    public List<ListAutomoveisResponse> listaTodosVeiculos() {
+        log.info("[inicia] AutomovelApplicationService - listaTodosVeiculos");
+        
+        log.info("[finaliza] AutomovelApplicationService - listaTodosVeiculos");
+        return null;
     }
 }
