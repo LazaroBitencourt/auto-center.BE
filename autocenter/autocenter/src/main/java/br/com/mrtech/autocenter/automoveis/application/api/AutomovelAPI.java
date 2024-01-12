7a1bb8b90ce033bcba3fa169c7e7057b63b623eb
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-01-12 19:45:30 -0300

    define verbo do metodo

--- autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/api/AutomovelAPI.java
+++ autocenter/autocenter/src/main/java/br/com/mrtech/autocenter/automoveis/application/api/AutomovelAPI.java
@@ -21,7 +21,7 @@ public interface AutomovelAPI {
     @ResponseStatus(HttpStatus.OK)
     DetalhaAutomovelResponse getDetalhaAutomovel(@PathVariable Long idAutomovel);
 
-    @GetMapping("{idAutomovel}")
+    @PatchMapping ("{idAutomovel}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void getAlteraInformacoesVeiculo(@PathVariable Long idAutomovel,AlteraInformacoes alteraInformacoes);
 }
